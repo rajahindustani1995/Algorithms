cd1cc5150823dc7234592abf35991e7a298edd47
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-06-28 16:13:03 +0530

    [Rajesh] Added : Permutation Of String

---
+++ Algorithm/Algorithm/PermutationOfString.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    internal class PermutationOfString
+    {
+        public static void Permute(String enteredString, int leftIndex, int rightIndex)
+        {
+            
+            if (leftIndex == rightIndex)
+            {
+                Console.WriteLine(enteredString);
+            }
+            
+            else
+            {
+                
+                for (int i = leftIndex; i <= rightIndex; i++)
+                {
+                    
+                    enteredString = Swap(enteredString, leftIndex, i);
+
+                    
+                    Permute(enteredString, leftIndex + 1, rightIndex);
+
+                    
+                    enteredString = Swap(enteredString, leftIndex, i);
+
+                }
+            }
+        }
+
+        
+        public static String Swap(String enteredString, int leftIndex, int rightIndex)
+        {
+            
+            char[] tempString = enteredString.ToCharArray();
+
+            
+            char temp = tempString[leftIndex];
+            tempString[leftIndex] = tempString[rightIndex];
+            tempString[rightIndex] = temp;
+
+            //Converting tempString array back to string
+            string toReturn = new string(tempString);
+
+            
+            return toReturn;
+        }
+
+        public void Driver()
+        {
+            Console.Write("Enter the string to find all of it's permutations : ");
+            string enteredString = Console.ReadLine();
+            Console.WriteLine("Permutations of the string " + enteredString + " are :");
+            Permute(enteredString, 0, enteredString.Length - 1);
+        }
+    }
+}
+
--- Algorithm/Algorithm/Program.cs
+++ Algorithm/Algorithm/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithms Program");
+            while (true)
+            {
+                Console.WriteLine("\n 1. Program to return all permutations of a string");
+
+                Console.WriteLine(" 0. Exit");
+                Console.Write("Enter your choice : ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        PermutationOfString permute = new PermutationOfString();
+                        permute.Driver();
+                        break;
+
+                    case 0:
+                        return;
+                    default:
+                        Console.WriteLine("Please choose the correct option");
+                        break;
+                }
+            }
+        }
+    }
+}
